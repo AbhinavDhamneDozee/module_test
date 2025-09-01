250d54a25a6ea3e38f743de54df92bd8eacdcfbf
Abhinav-Dozee <abhinavdhamne@dozee.io>
2025-09-02 00:09:11 +0530

    initial commit

---
+++ modules/hello_module/include/hello.h
@@ -0,0 +1,14 @@
+#ifndef HELLO_H__
+#define HELLO_H__
+
+/**
+ * @brief Initialize hello module.
+ */
+void hello_init(void);
+
+/**
+ * @brief Print a hello world log.
+ */
+void hello_print(void);
+
+#endif /* HELLO_H__ */
